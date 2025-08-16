b3e6ba5eff81bf7784ddd1b2f768530f68107918
Ayman Sabek <a.sabek313@gmail.com>
2025-08-15 20:53:39 -0400

    chore: remove nested duplicate directory

--- istampit-cli/tests/test_parser.py
+++
@@ -1,8 +0,0 @@
-from istampit_cli.__main__ import build_parser
-
-
-def test_help_contains_commands():
-    parser = build_parser()
-    help_text = parser.format_help()
-    for cmd in ["stamp", "verify", "upgrade", "info", "upgrade-all"]:
-        assert cmd in help_text
